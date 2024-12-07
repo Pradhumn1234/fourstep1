26a89b8528484704aaa8daf222d9cd01588ce77e
Pradh123 <vermapradhumn3@gmail.com>
2024-12-07 18:23:01 +0530

    new commit is here

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,39 +1,27 @@
 // src/utils/mongodb.js
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// let isConnected = false; // Track the connection status
+let isConnected = false; // Track the connection status
 
-// const dbConnect = async () => {
-//   if (isConnected) {
-//     console.log("Already connected to MongoDB");
-//     return;
-//   }
-//   try {
-//     const db = await mongoose.connect(MONGODB_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
+const dbConnect = async () => {
+  if (isConnected) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-//     isConnected = db.connections[0].readyState === 1;
-//     console.log("Connected to MongoDB");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//   }
-// };
-// export default dbConnect;
-import { MongoClient } from 'mongodb';
+    isConnected = db.connections[0].readyState === 1;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+export default dbConnect;
 
-export default async function handler(req, res) {
-
-    try {
-      // MongoDB connection (replace with your connection string)
-      await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
-    } catch (error) {
-      console.error('Error inserting industry:', error);
-      res.status(500).json({ message: 'Failed to add industry', error: error.message });
-    }
- 
-}
 
